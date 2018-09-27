5ff9420d5db8d7d91c17824646eb3d6d5bbbfb3d
Rohit Gupta <rohitguptarg24@gmail.com>
2018-09-27 15:38:07 -0400

    Initial Commit. 1. Game class for starting the Game. 2. Adding Game Window file.

---
+++ src/com/conquest/main/Game.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package com.conquest.main;
+
+import java.awt.Canvas;
+
+/**
+ * @author Rohit Gupta
+ *
+ */
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = -4669025547633011027L;
+	public static final int WIDTH =640;
+	public static final int HEIGHT = WIDTH/12 * 9;
+	private Thread thread;
+	private boolean isRunning = false;
+	public Game() {
+		new GameWindow(WIDTH,HEIGHT,"THE CONQUEST" , this);
+
+	}
+	
+	public static void main(String[] args) {
+
+	new Game();
+
+	}
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		isRunning = true;
+	}
+	
+	public synchronized void stop() {
+		
+		try {
+			thread.join();
+			isRunning = false;
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime)/ ns; 
+			lastTime = now;
+			while(delta >= 1)
+			{
+				tick();
+				delta--;
+			}
+			if(isRunning) {
+				render();
+			}
+			frames++;
+			if(System.currentTimeMillis() - timer > 1000) {
+				
+				timer +=1000;
+				System.out.println(" FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+		
+	}
+
+}
---
+++ src/com/conquest/main/GameWindow.java
@@ -0,0 +1,38 @@
+package com.conquest.main;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Rohit Gupta
+ *
+ */
+public class GameWindow extends Canvas{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 705136013521611381L;
+
+
+	public GameWindow(int width, int height, String title,Game game) {
+		
+		
+		JFrame jFrame = new JFrame(title);
+		jFrame.setPreferredSize(new Dimension(width, height));
+		jFrame.setMinimumSize(new Dimension(width, height));
+		jFrame.setMaximumSize(new Dimension(width, height));
+		
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setResizable(false);
+		jFrame.setLocationRelativeTo(null);
+		jFrame.add(game);
+		jFrame.setVisible(true);
+		game.start();
+		
+		
+		
+	}
+}
