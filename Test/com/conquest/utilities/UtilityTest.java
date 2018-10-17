9f8206cb7f47089112962407316217179b448877
bhigyapande <pande.1958@gmail.com>
2018-10-17 17:31:18 -0400

    test to parseAndValidateMap

---
+++ Test/com/conquest/utilities/UtilityTest.java
@@ -0,0 +1,56 @@
+package com.conquest.utilities;
+
+import static org.junit.Assert.*;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.conquest.mapeditor.model.CountryModel;
+
+public class UtilityTest {
+	Utility u=new Utility();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		ArrayList<CountryModel> parsedCountryList= u.parseAndValidateMap("E:\\Asia.map").getCountryList();
+		System.out.println(parsedCountryList.size());
+		ArrayList<String> countryNames=new ArrayList<String>();
+		ArrayList<String> countryList=new ArrayList<String>();
+		
+		for(CountryModel loopCountry : parsedCountryList)
+		{
+			countryNames.add(loopCountry.getCountryName());
+		}
+		System.out.println(countryNames+"hiiiii");
+		countryList.add("Russia");
+		countryList.add("Japan");
+		countryList.add("Georgia");
+		countryList.add("Armenia");
+		System.out.println(countryList);
+		assertTrue(countryList.equals(countryNames));
+	}
+
+}
