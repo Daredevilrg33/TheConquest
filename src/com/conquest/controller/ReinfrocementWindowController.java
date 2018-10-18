4e54329ec1954b7801857afa8fe0d4f41b555eb7
Siddhant <siddhantarora1996@gmail.com>
2018-10-18 18:00:29 -0400

    Improved functionality in fortification phase

--- src/com/conquest/controller/ReinfrocementWindowController.java
+++ src/com/conquest/controller/ReinfrocementWindowController.java
@@ -6,26 +6,34 @@ import com.conquest.mapeditor.model.PlayerModel;
 import com.conquest.window.GameWindow;
 import com.conquest.controller.GameWindowController;
 import com.conquest.window.ReinforcementWindow;
+import java.util.ArrayList;
 
 public class ReinfrocementWindowController {
 	private ReinforcementWindow reinforcementWindow;
 	private int noOfPlayers = 0;
 	private MapHierarchyModel mapModel;
+	private int armies;
 	private int sourceCounter = 0;
 	private int destinationCounter = 0;
 	private int counter = 0;
 	private PlayerModel[] players;
-	private int prevCounter = 0;
+//	private int prevCounter = 0;
+	private ArrayList<String> a = new ArrayList<>();
 
 	public ReinfrocementWindowController(PlayerModel[] players, ReinforcementWindow reinforcementWindow,
 			int noOfPlayers, MapHierarchyModel mapModel) {
 		this.reinforcementWindow = reinforcementWindow;
 		this.noOfPlayers = noOfPlayers;
 		this.mapModel = mapModel;
-		this.players = players;
 
-		updateUIInfo();
+		this.players =  players;
 
+		updateSourceUI();
+//		reinforcementWindow.updateSourceArmyLabel(players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry())
+//		reinforcementWindow.updateArmy(players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
+//		reinforcementWindow.updateComboBoxSourceCountries(players[counter].getPlayerCountryList());
+		reinforcementWindow.updatePlayerLabel(players[counter].getPlayerName());
+		
 	}
 
 //	private void initializingPlayerModels(int Players, MapHierarchyModel mapModel) {
@@ -34,59 +42,123 @@ public class ReinfrocementWindowController {
 //		updateUIInfo();
 //	
 //	}
-
-	public void sending(String sourceCountry, String destinationCountry, int armies) {
-
-		while (true) {
-			if (players[prevCounter].getPlayerCountryList().get(sourceCounter).getCountryName().trim()
-					.equalsIgnoreCase(sourceCountry.trim())) {
-				if (players[prevCounter].getPlayerCountryList().get(destinationCounter).getCountryName().trim()
-						.equalsIgnoreCase(destinationCountry.trim())) {
-					for (int s = 0; s < armies; s++) {
-						players[prevCounter].getPlayerCountryList().get(destinationCounter).addNoOfArmiesCountry();
-						players[prevCounter].getPlayerCountryList().get(sourceCounter).removeNoOfArmiesCountry();
-					}
-					System.out.println("New number of armies in destination"
-							+ players[prevCounter].getPlayerCountryList().get(destinationCounter).getCountryName()
-							+ " is " + players[prevCounter].getPlayerCountryList().get(destinationCounter)
-									.getNoOfArmiesCountry());
-					System.out.println("New number of armies in source"
-							+ players[prevCounter].getPlayerCountryList().get(sourceCounter).getCountryName() + " is "
-							+ players[prevCounter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
-					break;
-				}
-				destinationCounter++;
-				if (destinationCounter > players[prevCounter].getPlayerCountryList().size()) {
-					destinationCounter = 0;
-					sourceCounter = 0;
-					prevCounter++;
-				}
-			}
-			sourceCounter++;
-//			if (sourceCounter >= players[prevCounter].getPlayerCountryList().size()) {
-//				destinationCounter = 0;
-//				sourceCounter = 0;
-//				prevCounter++;
-//				}
+	
+	public void sending(int armies) {
+		for(int s=0; s<armies;s++) {
+		players[counter].getPlayerCountryList().get(destinationCounter).addNoOfArmiesCountry();
+		players[counter].getPlayerCountryList().get(sourceCounter).removeNoOfArmiesCountry();
 		}
-	}
-
-	public void updateUIInfo() {
-		prevCounter = counter;
-
-		reinforcementWindow
-				.updateArmy(players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
-		reinforcementWindow.updatePlayerLabel(players[counter].getPlayerName());
-		reinforcementWindow.updateArmyLabel(
-				players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry(),
-				players[counter].getPlayerCountryList().get(destinationCounter).getNoOfArmiesCountry());
-		reinforcementWindow.updateComboBoxSourceCountries(players[counter].getPlayerCountryList());
-		reinforcementWindow.updateComboBoxDestinationCountries(
-				players[counter].getPlayerCountryList().get(destinationCounter).getListOfNeighbours());
-		counter++;
-		if (counter == noOfPlayers)
+		System.out.println("Newnumber of armies in source: "+players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
+		System.out.println("Newnumber of armies in destination: "+players[counter].getPlayerCountryList().get(destinationCounter).getNoOfArmiesCountry());
+		if(counter<noOfPlayers-1)
+			counter++;
+		else
 			counter = 0;
+		
+		sourceCounter=0;
+		destinationCounter=0;
+		reinforcementWindow.newArmyLabel();
+//		a.clear();
+		reinforcementWindow.updateComboBoxSourceCountries(players[counter].getPlayerCountryList());
+		reinforcementWindow.updatePlayerLabel(players[counter].getPlayerName());
+//		updateSourceUI();
 
+	}
+	
+//	public void updatePlayer() {
+//		if(counter<=noOfPlayers)
+//			counter++;
+//		else
+//			counter =0;
+//	}
+	
+	public void finding(String sourceCountry) {
+
+		sourceCounter = 0 ;
+		destinationCounter = 0;
+//		while(true) {
+//			int i = 0;
+//			int j = 0;
+//			if(players[counter].getPlayerCountryList().get(sourceCounter).getCountryName().trim()
+//					.equalsIgnoreCase(sourceCountry.trim())) {
+//				System.out.println("country n first while loop "+players[counter].getPlayerCountryList().get(sourceCounter).getCountryName().trim());
+//				while(true) {
+//					if(players[counter].getPlayerCountryList().get(sourceCounter).getListOfNeighbours().get(i).trim()
+//						.equalsIgnoreCase(players[counter].getPlayerCountryList().get(destinationCounter).getCountryName().trim())) {
+//
+//					System.out.println("New number of armies in destination"+players[counter].getPlayerCountryList().get(destinationCounter).getCountryName()+" is "+players[counter].getPlayerCountryList().get(destinationCounter).getNoOfArmiesCountry());
+//					j++;
+//					a.add(players[counter].getPlayerCountryList().get(destinationCounter).getCountryName());
+//					System.out.println("New number of armies in source"+players[counter].getPlayerCountryList().get(sourceCounter).getCountryName()+" is "+players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
+//					break;
+//
+//				}
+//					destinationCounter++;
+//					if (destinationCounter >= players[counter].getPlayerCountryList().size()) {
+//						destinationCounter = 0;
+//						i++;
+//						}				
+//					}
+//				//redo with for loop
+//				if(j>0)
+//					break;
+//
+//				i=0;
+//			}
+//			sourceCounter++;
+//			}
+		
+		for(sourceCounter = 0; sourceCounter < players[counter].getPlayerCountryList().size(); sourceCounter++) {
+			int i = 0;
+			if(players[counter].getPlayerCountryList().get(sourceCounter).getCountryName().trim()
+					.equalsIgnoreCase(sourceCountry.trim())) {
+				System.out.println("country in first loop "+players[counter].getPlayerCountryList().get(sourceCounter).getCountryName().trim());	
+				for(destinationCounter=0; destinationCounter<players[counter].getPlayerCountryList().size() - 1;destinationCounter++) {
+					for(i = 0;i<players[counter].getPlayerCountryList().get(sourceCounter).getListOfNeighbours().size();i++) {
+						if(players[counter].getPlayerCountryList().get(sourceCounter).getListOfNeighbours().get(i).trim()
+								.equalsIgnoreCase(players[counter].getPlayerCountryList().get(destinationCounter).getCountryName())){
+							
+//							System.out.println("source Country is: "+players[counter].getPlayerCountryList().get(sourceCounter).getCountryName().trim());
+							System.out.println("destination Country is: "+players[counter].getPlayerCountryList().get(destinationCounter).getCountryName());
+							
+							a.add(players[counter].getPlayerCountryList().get(destinationCounter).getCountryName());
+
+						}
+					}
+
+				}
+				break;
+			}
+		}
+	
+	}
+		
+	public void updateDestinationUI(String selectedDestination) {
+		for(int i=0; i<players[counter].getPlayerCountryList().size();i++) {
+			if(players[counter].getPlayerCountryList().get(i).getCountryName().equalsIgnoreCase(selectedDestination)) {
+				destinationCounter = i;
+				break;
+			}
+		}
+		
+		reinforcementWindow.updateDestinationArmyLabel(players[counter].getPlayerCountryList().get(destinationCounter).getNoOfArmiesCountry());		
+		
+	}
+	
+	public void updateSourceUI() {
+		reinforcementWindow.updateComboBoxSourceCountries(players[counter].getPlayerCountryList());
+
+	}
+	
+	public void updateUIInfo() {
+//		prevCounter = counter;
+
+		reinforcementWindow.updateArmy(players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
+		reinforcementWindow.updateSourceArmyLabel(players[counter].getPlayerCountryList().get(sourceCounter).getNoOfArmiesCountry());
+//		reinforcementWindow.updateComboBoxSourceCountries(players[counter].getPlayerCountryList());
+//		reinforcementWindow.updateComboBoxDestinationCountries(players[counter].getPlayerCountryList().get(destinationCounter).getListOfNeighbours());
+		reinforcementWindow.updateComboBoxDestinationCountries(a);
+		a.clear();
 	}
 
 }
--- src/com/conquest/window/ReinforcementWindow.java
+++ src/com/conquest/window/ReinforcementWindow.java
@@ -2,6 +2,8 @@ package com.conquest.window;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.ArrayList;
@@ -11,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import com.conquest.controller.ReinfrocementWindowController;
 import com.conquest.mapeditor.model.CountryModel;
@@ -49,10 +52,13 @@ public class ReinforcementWindow extends JFrame implements ActionListener {
 
 		jComboBoxSourceCountries = new JComboBox<>();
 		jComboBoxSourceCountries.setBounds(170, 50, 100, 30);
+
 		add(jComboBoxSourceCountries);
+
 		jComboBoxDestinationCountries = new JComboBox<>();
 		jComboBoxDestinationCountries.setBounds(300, 50, 100, 30);
 		add(jComboBoxDestinationCountries);
+
 		jButtonSend = new JButton("Send");
 		jButtonSend.setBounds(600, 50, 100, 30);
 		jButtonSend.addActionListener(this);
@@ -133,6 +139,9 @@ public class ReinforcementWindow extends JFrame implements ActionListener {
 
 			}
 		});
+		jComboBoxSourceCountries.addActionListener(this);
+		jComboBoxDestinationCountries.addActionListener(this);
+
 	}
 
 	public void updateArmy(int number) {
@@ -149,16 +158,29 @@ public class ReinforcementWindow extends JFrame implements ActionListener {
 		jPlayerLabel.setText(playerName);
 	}
 
-	public void updateArmyLabel(int sourceArmy, int DestinationArmy) {
+	public void updateSourceArmyLabel(int sourceArmy) {
 //		System.out.println("updatePlayerLabel Value: "+  playerName);
 		jSourceArmyLabel.setText(Integer.toString(sourceArmy));
-		jDestinationArmyLabel.setText(Integer.toString(DestinationArmy));
+//		jDestinationArmyLabel.setText(Integer.toString(DestinationArmy));
+
+	}
+
+	public void newArmyLabel() {
+		jSourceArmyLabel.setText("");
+		jDestinationArmyLabel.setText("");
+	}
+
+	public void updateDestinationArmyLabel(int destinationArmy) {
+//		System.out.println("updatePlayerLabel Value: "+  playerName);
+		jDestinationArmyLabel.setText(Integer.toString(destinationArmy));
+//		jDestinationArmyLabel.setText(Integer.toString(DestinationArmy));
 
 	}
 
 	public void updateComboBoxSourceCountries(List<CountryModel> countryModels) {
 
 		jComboBoxSourceCountries.removeAllItems();
+		jComboBoxSourceCountries.addItem("Select country:");
 		for (CountryModel countryModel : countryModels) {
 //			System.out.println("updateComboBoxCountries Value: "+  countryModel.getCountryName());
 
@@ -170,7 +192,9 @@ public class ReinforcementWindow extends JFrame implements ActionListener {
 	public void updateComboBoxDestinationCountries(ArrayList<String> arrayList) {
 
 		jComboBoxDestinationCountries.removeAllItems();
-		for (int i = 0; i < arrayList.size(); i++) {
+		jComboBoxDestinationCountries.addItem("Select country:");
+
+		for (int i = arrayList.size() - 1; i >= 0; i--) {
 //			System.out.println("updateComboBoxCountries Value: "+  countryModel.getCountryName());
 
 			jComboBoxDestinationCountries.addItem(arrayList.get(i));
@@ -181,24 +205,45 @@ public class ReinforcementWindow extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		switch (e.getActionCommand()) {
-		case "Send":
+
+		if (e.getSource() == jButtonSend) {
 			System.out.println("Selected Player Name: " + jPlayerLabel.getText().toString());
 			System.out
 					.println("Selected Source Country Name: " + jComboBoxSourceCountries.getSelectedItem().toString());
-			System.out.println(
-					"Selected Destination Country Name: " + jComboBoxDestinationCountries.getSelectedItem().toString());
+//			System.out.println("Selected Destination Country Name: " + jComboBoxDestinationCountries.getSelectedItem().toString());
 
-			selectedSourceCountry = jComboBoxSourceCountries.getSelectedItem().toString();
-			selectedDestinationCountry = jComboBoxDestinationCountries.getSelectedItem().toString();
+			if (jComboBoxSourceCountries.getSelectedItem().toString().equalsIgnoreCase("Select Country:")) {
 
-			jComboBoxChosenNoOfArmies.getSelectedItem();
-			reinforcementWindowController.sending(selectedSourceCountry, selectedDestinationCountry, armies);
-			reinforcementWindowController.updateUIInfo();
+				JOptionPane.showMessageDialog(this, "Select Country to continue.", "Error Message",
+						JOptionPane.ERROR_MESSAGE);
 
-			break;
-		default:
-			break;
+			} else {
+
+				armies = Integer.valueOf(jComboBoxChosenNoOfArmies.getSelectedItem().toString());
+				reinforcementWindowController.sending(armies);
+//			reinforcementWindowController.updateSourceUI();
+//			reinforcementWindowController.updatePlayer();
+//			reinforcementWindowController.updateUIInfo();	   
+			}
+
+		} else if (e.getSource() == jComboBoxSourceCountries) {
+			System.out.println("JCombo Source COuntry selected");
+			if (jComboBoxSourceCountries.getItemCount() != 0) {
+				selectedSourceCountry = jComboBoxSourceCountries.getSelectedItem().toString();
+//			selectedDestinationCountry = jComboBoxDestinationCountries.getSelectedItem().toString();
+				reinforcementWindowController.finding(selectedSourceCountry);
+				reinforcementWindowController.updateUIInfo();
+			}
+		} else if (e.getSource() == jComboBoxDestinationCountries) {
+			if (jComboBoxDestinationCountries.getItemCount() != 0) {
+				selectedDestinationCountry = jComboBoxDestinationCountries.getSelectedItem().toString();
+			} else {
+				selectedDestinationCountry = "";
+			}
+			reinforcementWindowController.updateDestinationUI(selectedDestinationCountry);
+
 		}
+
 	}
+
 }
