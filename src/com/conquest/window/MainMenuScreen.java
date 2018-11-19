f69df85661fbf3606469a755a7c72c034ebac06c
nancygoyal1206 <goyal.nancy1206@gmail.com>
2018-11-18 20:03:42 -0500

    Logging implementation started

--- src/com/conquest/window/MainMenuScreen.java
+++ src/com/conquest/window/MainMenuScreen.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 
 import com.conquest.mapeditor.view.MapDashboard;
 import com.conquest.utilities.Constants;
+import org.apache.log4j.*;
 
 /**
  * The Class MainMenuScreen.
@@ -32,33 +33,35 @@ public class MainMenuScreen extends JFrame implements ActionListener {
 
 	/** The button exit. */
 	private JButton btnExit;
+	
+	private static final Logger log = Logger.getLogger(MainMenuScreen.class);
 
 	/**
 	 * Instantiates a new main menu screen.
 	 */
 	public MainMenuScreen() {
-		btnNewSingleGame = new JButton("New Game");
-		btnNewSingleGame.setBounds(Constants.WIDTH / 2 - 50, 50, 100, 30);
+		btnNewSingleGame = new JButton("New Single Game");
+		btnNewSingleGame.setBounds(Constants.WIDTH / 2 - 50, 50, 150, 30);
 		btnNewSingleGame.addActionListener(this);
 		add(btnNewSingleGame);
 
 		btnNewTournament = new JButton("New Tournament");
-		btnNewTournament.setBounds(Constants.WIDTH / 2 - 50, 120, 100, 30);
+		btnNewTournament.setBounds(Constants.WIDTH / 2 - 50, 120, 150, 30);
 		btnNewTournament.addActionListener(this);
 		add(btnNewTournament);
 
 		btnMapEditor = new JButton("Map Editor");
-		btnMapEditor.setBounds(Constants.WIDTH / 2 - 50, 190, 100, 30);
+		btnMapEditor.setBounds(Constants.WIDTH / 2 - 50, 190, 150, 30);
 		btnMapEditor.addActionListener(this);
 		add(btnMapEditor);
 
 		btnLoadGame = new JButton("Load Game");
-		btnLoadGame.setBounds(Constants.WIDTH / 2 - 50, 260, 100, 30);
+		btnLoadGame.setBounds(Constants.WIDTH / 2 - 50, 260, 150, 30);
 		btnLoadGame.addActionListener(this);
 		add(btnLoadGame);
 		
 		btnExit = new JButton("Exit");
-		btnExit.setBounds(Constants.WIDTH / 2 - 50, 330, 100, 30);
+		btnExit.setBounds(Constants.WIDTH / 2 - 50, 330, 150, 30);
 		btnExit.addActionListener(this);
 		add(btnExit);
 		setTitle("Main Menu");
@@ -66,6 +69,8 @@ public class MainMenuScreen extends JFrame implements ActionListener {
 		setSize(Constants.WIDTH, Constants.HEIGHT);
 		setLayout(null);
 		setLocationRelativeTo(null);
+		
+		
 
 		addWindowListener(new WindowAdapter() {
 			/*
@@ -90,18 +95,22 @@ public class MainMenuScreen extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 
 		if (event.getSource() == btnNewSingleGame) {
+			log.info("New Single Game started");
 			NewGameMenuScreen secondScreen = new NewGameMenuScreen();
 			secondScreen.setVisible(true);
 			dispose();
 		} else if (event.getSource() == btnMapEditor) {
+			log.info("Map Editor launched");
 			MapDashboard mapDashboard = new MapDashboard();
 			mapDashboard.setVisible(true);
 			dispose();
 		} else if (event.getSource() == btnNewTournament) {
+			log.info("New Tournament started");
 			NewTournamentMenuScreen newTournamentScreen = new NewTournamentMenuScreen();
 			newTournamentScreen.setVisible(true);
 			dispose();
 		}else if (event.getSource() == btnLoadGame) {
+			log.info("Game loaded");
 			LoadGameWindow loadGameWindow = new LoadGameWindow();
 //				loadGameWindow.setVisible(true);
 			dispose();
